529fdbdbcce5fc1b373d71ed6a30542b6499e8d3
Pirozoni <pirozoni@yandex.ru>
2024-10-25 16:38:11 +0300

    Initial Commit

---
+++ imageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imageApp
+//
+//  Created by Надежда Пономарева on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ imageAppTests/imageAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  imageAppTests.swift
+//  imageAppTests
+//
+//  Created by Надежда Пономарева on 25.10.2024.
+//
+
+import Testing
+@testable import imageApp
+
+struct imageAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
