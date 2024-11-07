32372f82e3cecf38006495cc6082fceebc727d51
Pirozoni <pirozoni@yandex.ru>
2024-11-07 12:49:57 +0300

    Added UI -Image, -label, -button, fixed constraints

---
+++ imageApp/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,41 @@
+//  ViewController.swift
+//  imageApp
+//
+//  Created by Надежда Пономарева on 25.10.2024.
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 200
+    }
+    
+    func configCell(for cell: ImagesListCell) {
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
