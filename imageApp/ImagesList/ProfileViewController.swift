0917c5b1a5e9af27038853b615297e3a181521c7
Pirozoni <pirozoni@yandex.ru>
2024-11-09 14:50:54 +0300

    Created profile screen and added constraints

---
+++ imageApp/ImagesList/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//  ProfileViewController.swift
+//  imageApp
+//
+//  Created by Надежда Пономарева on 09.11.2024.
+
+import UIKit
+
+class ProfileViewController {
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var exitIcon: UIButton!
+    @IBOutlet weak var profileUserName: UILabel!
+    @IBOutlet weak var profileUserNickname: UILabel!
+    @IBOutlet weak var profileUserText: UILabel!
+}
