dc439b863027d7cbc8bef41b82edbc5b0dae300b
Pirozoni <pirozoni@yandex.ru>
2024-12-05 20:59:03 +0300

    Created func rescaleAndCenterImageInScrollView which help user to open and zoom image

--- imageApp/SingleImageViewController.swift
+++ imageApp/SingleImageViewController.swift
@@ -7,21 +7,59 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    // MARK: - IB Outlets
     @IBOutlet private var imageView: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        imageView.image = image
+    @IBOutlet private var scrollView: UIScrollView!
+    
+    // MARK: - Methods
+    func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
+    // MARK: - Overrides Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let image else { return }
+        imageView.image = image
+        imageView.frame.size = image.size
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        rescaleAndCenterImageInScrollView(image: image)
+    }
     
+    // MARK: - IB Action
     @IBAction private func backButton(_ sender: Any) {
         dismiss( animated: true, completion: nil)
+    }
+}
+
+    // MARK: - Extensions
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
 
